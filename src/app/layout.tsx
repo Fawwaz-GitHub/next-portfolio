4406d54793bf12b53fa0deb023b8ed74e8929d75
Fawwaz <mohammed.fawwaz@xencia.com>
2025-11-07 23:04:54 +0530

    font and metadata updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,44 +1,28 @@
 import "./globals.scss";
-import { Poppins } from "next/font/google";
+import { Manrope } from "next/font/google";
 import { ReactNode } from "react";
 import { Metadata } from "next";
 import dynamic from "next/dynamic";
 import { navMenus } from "@/data/navMenus";
 
-const poppins = Poppins({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  style: ["normal", "italic"],
+const manrope = Manrope({
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
   subsets: ["latin", "latin-ext"],
   display: "swap",
   preload: true,
-  fallback: [
-    "system-ui",
-    "arial",
-    "BlinkMacSystemFont",
-    "Segoe UI",
-    "Roboto",
-    "Oxygen",
-    "Ubuntu",
-    "Fira Sans",
-    "Droid Sans",
-  ],
+  fallback: ["system-ui", "sans-serif"],
 });
 
 export const metadata: Metadata = {
-  title: "Nikhil Rajput",
+  title: "Mohammed Fawwaz - Full Stack Developer | SDE | Programmer",
   description:
-    "Nikhil Rajput is a proficient Software Engineer and Full Stack Developer from India, skilled in front-end and back-end development using modern tech stacks.",
+    "Mohammed Fawwaz is a proficient Software Engineer and Full Stack Developer from India, skilled in front-end and back-end development using modern tech stacks.",
   keywords: [
-    "nikhil rajput",
-    "nikhil",
-    "nixrajput",
-    "nikhil-rajput",
-    "rajput nikhil",
-    "founder of nixlab",
-    "nixlab founder",
+    "mohammed fawwaz",
+    "fawwaz",
     "full stack developer",
     "indian developer",
-    "nixrajput github",
+    "fawwaz github",
   ],
 };
 
@@ -49,7 +33,7 @@ const ScrollToTop = dynamic(() => import("@/components/common/ScrollToTop"));
 
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
-    <html lang="en" className={poppins.className}>
+    <html lang="en" className={manrope.className}>
 
       <body>
         <FloatingNavbar className="app_nav" navItems={navMenus} />
